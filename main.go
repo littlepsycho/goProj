1fbf82e982faef011e0cd45628931b8cdfc830bb
littlepsycho <littlepsycho@163.com>
2018-07-16 22:14:00 +0800

    var&&value

--- main.go
+++ main.go
@@ -2,7 +2,34 @@ package main
 
 import "fmt"
 
-func main(){
-    fmt.Println("git test3")
-    fmt.Println("git test4")
+func ExmplValue() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+func ExmplVar() {
+	var a string = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
+
+func main() {
+	ExmplValue()
+	ExmplVar()
 }
