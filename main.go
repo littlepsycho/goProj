e54e76038e841b31d64aaf7a729f56902fa49ace
littlepsycho <littlepsycho@163.com>
2018-07-21 15:31:37 +0800

    range func

--- main.go
+++ main.go
@@ -8,6 +8,125 @@ import (
 
 const s string = "constant"
 
+func ExmplRange() {
+	nums := []int{2, 3, 4}
+
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+
+	for k, v := range kvs {
+		fmt.Printf("%s->%s\n", k, v)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+}
+
+func ExmplMap() {
+	m := make(map[string]int)
+
+	m["k1"] = 7
+	m["k2"] = 10
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("V1:", v1)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+	fmt.Println(n["foo"])
+}
+
+func ExmplSlice() {
+	s := make([]string, 3)
+	fmt.Println("emp:", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+
+	fmt.Println("2d:", twoD)
+}
+
+func ExmplArray() {
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+
+	fmt.Println("2d:", twoD)
+}
+
 func ExmplSwitch() {
 	i := 2
 	fmt.Print("Write", i, " as ")
@@ -117,10 +236,15 @@ func ExmplValue() {
 
 func main() {
 
+	fmt.Println("go Demo")
 	ExmplValue()
 	ExmplVar()
 	ExmplConst()
 	ExmplFor()
 	ExmplIfElse()
 	ExmplSwitch()
+	ExmplArray()
+	ExmplSlice()
+	ExmplMap()
+	ExmplRange()
 }
